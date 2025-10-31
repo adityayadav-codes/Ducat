e3326a6925621dd6eec25772ddee04b260525e99
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-31 23:47:29 +0530

    10th problem

--- Temp0.java
+++ Temp0.java
@@ -24,23 +24,62 @@
 // }
 
 // 8.
-public class Temp0{
-    public static void main (String args [])	{
+// public class Temp0{
+//     public static void main (String args [])	{
 	
-		double GPA = 7.5;
-		int attendance = 70;
-		 boolean  isEligible = SEC(GPA , attendance);
+// 		double GPA = 7.5;
+// 		int attendance = 70;
+// 		 boolean  isEligible = SEC(GPA , attendance);
 			
-			if(isEligible)	{
+// 			if(isEligible)	{
 			
-				System.out.println("You r eligible  for scholarship!");
-	}	else {
+// 				System.out.println("You r eligible  for scholarship!");
+// 	}	else {
 				
-				System.out.println("you r not eligible");
-	}
-	}
-     public static boolean SEC(double GPA, int attendance){
+// 				System.out.println("you r not eligible");
+// 	}
+// 	}
+//      public static boolean SEC(double GPA, int attendance){
         
-            return (GPA >=7.0 && attendance >=75);
-	}
-}
+//             return (GPA >=7.0 && attendance >=75);
+// 	}
+// }
+
+// 9.
+// public class Temp0{
+//     public static void main(String args []){
+// 	int num = 15;
+		
+// 		if(num % 3 ==0 && num % 5==0)	{
+		
+// 			System.out.print("this number is multiple of 3 and 5 is: ");
+// 			System.out.println(+num);
+// 	} else {
+		
+// 			System.out.println("it’s not");
+// 	}
+// }
+ //}
+ // 10.
+// a java program  to check if a string contains a specific substring
+ public class Temp0{
+     public static void main (String args [])	{
+    
+        String str = "Hello, welcome to the world of Java programming.";
+        String substring = "Java";
+         boolean  containsSubstring = checkSubstring(str , substring);
+            
+            if(containsSubstring)	{
+            
+                System.out.println("The string contains the substring: " + substring);
+    }	else {
+                
+                System.out.println("The string does not contain the substring: " + substring);
+    }
+    }
+     public static boolean checkSubstring(String str, String substring){
+        
+            return str.contains(substring);
+            
+    }
+ }
