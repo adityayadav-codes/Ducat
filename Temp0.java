87eb8c16e1a28253376acdb5f18f0cf13dd60a1a
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-31 23:20:25 +0530

    run 7 code

--- Temp0.java
+++ Temp0.java
@@ -1,13 +1,24 @@
-public class Temp0 {
-    public static void main(String args []){
-        int num = 200;
-        System.out.println("the number is :"+num);
-        if(num >=1 && num <=100){
-            System.out.println("the number is in the range of 1 to 100 ");
+// 
+// 7.
+// a java program to check if a student is eligible for a load based on their income and credit score
+public class Temp0{
+    public static void main (String args [])	{
+    
+        double income = 50000;
+        double creditScore = 880;
+         boolean  isEligible = cla(income , creditScore);
+    
+    
+            if(isEligible)	{
         
-        }else {
-            System.out.println("the number is out of range");
-        }
-
+                System.out.println("Your are eligible for load");
+    } else {
+                
+                System.out.println("YOU r not eligible");	
+    }
+    }
+     public static boolean cla(double income, double creditScore){
+        
+            return (income >=50000 && creditScore >=500);
     }
 }
