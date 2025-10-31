3d4dc4d86e6eef1cf8c4a37efd570be181c95028
adityayadav-codes <adityayadav2905@gmail.com>
2025-10-31 22:55:20 +0530

    add files for doing practive

---
+++ Temp0.java
@@ -0,0 +1,13 @@
+public class Temp0 {
+    public static void main(String args []){
+        int num = 200;
+        System.out.println("the number is :"+num);
+        if(num >=1 && num <=100){
+            System.out.println("the number is in the range of 1 to 100 ");
+        
+        }else {
+            System.out.println("the number is out of range");
+        }
+
+    }
+}
