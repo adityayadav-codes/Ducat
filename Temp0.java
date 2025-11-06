9b77869b09d9fee6e7aa1b824d7b063ccfec9cd2
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-06 23:00:55 +0530

    practice pattern

--- Temp0.java
+++ Temp0.java
@@ -42,18 +42,61 @@
     * * * *
     * * * *
     * * * *
+//  */
+// public class Temp0 {
+
+//     public static void main (String args []) {
+
+//         for(int i = 1; i <= 4; i++) {
+
+//             for(int j = 1; j <= 4; j++) {
+
+//                 System.out.print("*" + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+/*
+    1
+    1 2 
+    1 2 3 
+    1 2 3 4 
+//  */
+// public class  Temp0{
+
+//     public static void main(String[] args) {
+        
+//         for(int i = 1; i <= 4; i++) {
+
+//                 if(i==3){
+//                     continue;
+//                 }
+//             for(int j = 1; j <= i; j++) {
+
+//                 System.out.print(j + " ");
+            
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+/*
+    * * * *
+    * * *
+    * *
+    *
  */
 public class Temp0 {
 
-    public static void main (String args []) {
+    public static void main(String[] args) {
+         for(int i = 4; i >= 1; i--) {
 
-        for(int i = 1; i <= 4; i++) {
-
-            for(int j = 1; j <= 4; j++) {
+            for(int j = 1; j <= i; j++) {
 
                 System.out.print("*" + " ");
             }
             System.out.println();
-        }
+         }
     }
 }
