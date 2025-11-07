a5f30afb88082767fc251937473f6c73949d14b7
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-07 23:03:35 +0530

    one more problem

--- Main.java
+++ Main.java
@@ -28,13 +28,59 @@
 //     }
 // }
 
-public class Main {
-    public static void main(String[] args) {
+// public class Main {
+//     public static void main(String[] args) {
         
     
-    int  [] arr = {10, 20, 30, 40, 50};
-        for(int i : arr){
-            System.out.println(i);
+//     int  [] arr = {10, 20, 30, 40, 50};
+//         for(int i : arr){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+// //max
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int [] arr = {10,20,30,40,50};
+//         int max = arr[0];
+//         for(int i =0; i < arr.length; i++) {
+//             if(arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+//         System.out.println(max);
+//     }
+// }
+// // min
+// public class Main {
+//         public static void main(String[] args) {
+//             int [] arr = {10, 20, 30, 40, 5};   
+//                 int min = arr[0];
+//                for(int i =0; i <arr.length;i++){
+//                     if(arr[i] < min) {
+//                         min = arr[i];
+//                     }
+//                 }
+//                 System.out.println(min);
+            
+//         }
+// }
+//average
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        double average;
+        int []arr = {10,20,30,40,50};
+        for(int i =0; i < arr.length; i++) {
+            sum += arr[i];
+              
         }
+        average = (double)sum/arr.length; 
+         System.out.println("average: "+average);
     }
 }
