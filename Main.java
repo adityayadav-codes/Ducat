d14bf39b014bfeac0e2333b90023e53f951b6de0
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-07 22:44:43 +0530

    practice for improving

--- Main.java
+++ Main.java
@@ -1,29 +1,40 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 20;
+//            void addition(){
+//             return a + b;
+//          }
+//           void subtraction() {
+//             return b - a;
+//          }
+//          int choice = 2;
+//          do {
+//             switch (choice) {
+//                 case 1:
+//                     {   
+//                         System.out.println(addition());
+//                         break;
+//                     }  
+//                     case 2:
+//                     {   
+//                         System.out.println(subtraction());
+//                         break;
+//                     }      
+//                     default:
+//                     break;
+//             }
+//          } while (choice>=0);
+//     }
+// }
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-           void addition(){
-            return a + b;
-         }
-          void subtraction() {
-            return b - a;
-         }
-         int choice = 2;
-         do {
-            switch (choice) {
-                case 1:
-                    {   
-                        System.out.println(addition());
-                        break;
-                    }  
-                    case 2:
-                    {   
-                        System.out.println(subtraction());
-                        break;
-                    }      
-                    default:
-                    break;
-            }
-         } while (choice>=0);
+        
+    
+    int  [] arr = {10, 20, 30, 40, 50};
+        for(int i : arr){
+            System.out.println(i);
+        }
     }
 }
