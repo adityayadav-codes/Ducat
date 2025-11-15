7f6d82cf588a9c48d7872e70fe23320b2bad73ce
adityayadav-codes <adityayadav2905@gmail.com>
2025-11-15 23:27:46 +0530

    update code

--- Google.java
+++ Google.java
@@ -35,46 +35,46 @@
 
 //static data member
 
-// public  class Google{
+public  class Google{
 
-//     int id;
-//     String name;
-//     int salary;
-//     static String cName = "Google";
+    int id;
+    String name;
+    int salary;
+    static String cName = "Google";
 
-//         void store(int id1, String name1, int salary1) {
+        void store(int id1, String name1, int salary1) {
 
-//             id = id1;
-//             name = name1;
-//             salary = salary1;
-//         }
-//         //consoole
-//         void show() {
-//             System.out.println("=============================");
-//             System.out.println("ID              : "+id);
-//             System.out.println("Name            : "+name);
-//             System.out.println("Salalry         : "+salary);
-//             System.out.println("Company Name    : "+cName);
+            id = id1;
+            name = name1;
+            salary = salary1;
+        }
+        //consoole
+        void show() {
+            System.out.println("=============================");
+            System.out.println("ID              : "+id);
+            System.out.println("Name            : "+name);
+            System.out.println("Salalry         : "+salary);
+            System.out.println("Company Name    : "+cName);
 
-//         }
-//     public static void main(String args []) {
+        }
+    public static void main(String args []) {
 
-//             // Employee 1:
-//             Google emp1 = new Google();
-//             emp1.store(101, "Aadi", 23232);
-//             emp1.show();
+            // Employee 1:
+            Google emp1 = new Google();
+            emp1.store(101, "Aadi", 23232);
+            emp1.show();
 
-//             //Employee 2:
-//             Google emp2 = new Google();
-//             emp2.store(102, "Akki", 4342);
-//             emp2.show();
+            //Employee 2:
+            Google emp2 = new Google();
+            emp2.store(102, "Akki", 4342);
+            emp2.show();
 
-//             //Employee 3:
-//             Google emp3 = new Google();
-//             emp3.store(103, "Anil", 5433);
-//             emp3.show();
-//     }
-// }
+            //Employee 3:
+            Google emp3 = new Google();
+            emp3.store(103, "Anil", 5433);
+            emp3.show();
+    }
+}
 
 
 // Data Shadowing
