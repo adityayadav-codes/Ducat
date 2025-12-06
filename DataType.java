77eec0dbb023def4022055b5f812e57974a0fdea
adityayadav-codes <adityayadav2905@gmail.com>
2025-12-06 12:26:08 +0530

    add file

---
+++ DataType.java
@@ -0,0 +1,22 @@
+//class Level Variable
+
+// public  class DataType {
+// 	static int  x;
+// 	static float f;
+// 	static boolean b;
+// 	static char ch;
+// 	public static void main (String args []) {
+	
+// 		System.out.println(x);
+// 		System.out.println(b);
+// 		System.out.println(ch);
+// }
+// }
+
+// local Level Variable
+public class DataType{
+    public static void main(String[] args) {
+        int x ;
+        System.out.println(x);
+    }
+}
