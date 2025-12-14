e59f047383efe7bbc3f9ee598a88db7cc5dd4fb0
adityayadav-codes <adityayadav2905@gmail.com>
2025-12-14 12:28:17 +0530

    add Files

---
+++ Decision Making/Demo.java
@@ -0,0 +1,14 @@
+public class Demo { 
+	public static void main(String args [] ){
+		
+		int num = 10;
+			
+		if(num >=0) {
+		
+			System.out.println("Positive number");
+		}
+		else {
+			System.out.println("Negative number");
+		}
+	}
+}
