d0c88bd9b15a1fb4aedd09e8f801570aa1f56a9e
adityayadav-codes <adityayadav2905@gmail.com>
2025-12-14 12:37:21 +0530

    3 program

--- Decision Making/Demo.java
+++ Decision Making/Demo.java
@@ -14,17 +14,33 @@
 // }
 
 // 2.
+// public class Demo {
+	
+// 	public static void main(String args [] ) {
+// 		int age = 18;
+	
+// 			if(age >= 18) {
+				
+// 				System.out.println("You r eligible for voting");
+// 		} 	else {
+			
+// 				System.out.println("You r not eligible  for voting");
+// 		}
+// 	}
+// }
+
+// 3.
 public class Demo {
 	
-	public static void main(String args [] ) {
-		int age = 18;
-	
-			if(age >= 18) {
-				
-				System.out.println("You r eligible for voting");
-		} 	else {
+	public static void main (String args [] ) {
+		
+		int num =0;
 			
-				System.out.println("You r not eligible  for voting");
+			if(num % 2 == 0) {
+				
+				System.out.println("Even Number ");
+		}	else  {
+				System.out.println("Odd Number ");
 		}
 	}
 }
