576fb5991792c5052549741391aae2ff30572ff3
adityayadav-codes <adityayadav2905@gmail.com>
2025-12-14 12:30:51 +0530

    2 Program

--- Decision Making/Demo.java
+++ Decision Making/Demo.java
@@ -1,14 +1,30 @@
-public class Demo { 
-	public static void main(String args [] ){
+// public class Demo { 
+// 	public static void main(String args [] ){
 		
-		int num = 10;
+// 		int num = 10;
 			
-		if(num >=0) {
+// 		if(num >=0) {
 		
-			System.out.println("Positive number!!");
-		}
-		else {
-			System.out.println("Negative number!!");
+// 			System.out.println("Positive number!!");
+// 		}
+// 		else {
+// 			System.out.println("Negative number!!");
+// 		}
+// 	}
+// }
+
+// 2.
+public class Demo {
+	
+	public static void main(String args [] ) {
+		int age = 18;
+	
+			if(age >= 18) {
+				
+				System.out.println("You r eligible for voting");
+		} 	else {
+			
+				System.out.println("You r not eligible  for voting");
 		}
 	}
 }
