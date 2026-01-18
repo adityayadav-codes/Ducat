ec1c646b8a4c47a63b83690bbc183e3a7fdeefe2
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-18 22:17:32 +0530

    A program to demonstrate Parametirized constructor

--- Constructor/cons.java
+++ Constructor/cons.java
@@ -1,1 +1,17 @@
 // Paramertrized constructor example in Java
+public class cons{
+    int id;
+    String name;
+     
+    cons(int id1, String name1) {
+        id = id1;
+        name = name1;
+    }
+    void show() {
+        System.out.println("ID :" + id +"\nName : "+name);
+    }
+    public static void main(String [] args ) {
+        cons stu1 = new cons(230714, "Alice Parry");
+        stu1.show();
+    }
+}
