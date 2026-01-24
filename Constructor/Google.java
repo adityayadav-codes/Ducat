919e094a3e71c03426352ae3aa6030068534f7a8
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-24 14:17:31 +0530

    A Program to demonstrate Parameterized Constructor

--- Constructor/Google.java
+++ Constructor/Google.java
@@ -1,27 +1,56 @@
 // default Constructor
+// public class Google {
+//     int id;
+//     String name;
+//     int salary;
+// static String cname = "Google";
+
+//     Google() {
+//         id = 101;
+//         name = "Aditya Yadadv";
+//         salary = 34343;
+//     }
+//     void display(){
+//         System.out.println("Id : "+id);
+//         System.out.println("Name : "+name);
+//         System.out.println("Salary : "+salary);
+//         System.out.println("Comapany Name : "+cname);
+//     }
+//     public static void main(String[] args) {
+//         Google emp1 = new Google();
+//         emp1.display();
+//         Google emp2 = new Google();
+//         emp2.display();
+//         Google emp3 = new Google();
+//         emp3.display();
+//     }
+// }
+
+//  Parameterized Constructor
+
 public class Google {
     int id;
     String name;
     int salary;
-static String cname = "Google";
+    static String cname = "Google";
 
-    Google() {
-        id = 101;
-        name = "Aditya Yadadv";
-        salary = 34343;
+    Google (int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
     }
-    void display(){
+    void display() {
         System.out.println("Id : "+id);
         System.out.println("Name : "+name);
         System.out.println("Salary : "+salary);
-        System.out.println("Comapany Name : "+cname);
+        System.out.println("Company Name : "+cname);
     }
-    public static void main(String[] args) {
-        Google emp1 = new Google();
+    public static void main(String [] args) {
+        Google emp1 = new Google(101, "Aditya Yadav", 34323);
         emp1.display();
-        Google emp2 = new Google();
+        Google emp2 = new Google(102, "Akash Yadav", 343225);
         emp2.display();
-        Google emp3 = new Google();
+        Google emp3 = new Google(103, "Anil Singh", 645434);
         emp3.display();
     }
 }
