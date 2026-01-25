52d0de4e47238c8d9e5672016e9c31be77be8394
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-25 17:27:17 +0530

    Inheritance in jaava

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -1,5 +1,18 @@
-package Inheritance;
-
+class A {
+    void parent(){
+        System.out.println("Parennt Class!");
+    }
+}
+class B  extends A{
+    void child() {
+        System.out.println("Child Class!");
+    }
+}
 public class Main {
+    public static void main(String[] args) {
+        B b1 = new B();
+        b1.child();
+        b1.parent();
+    }
     
 }
