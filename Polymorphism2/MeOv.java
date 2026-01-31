49844188c132b02152a9779c7a9ecd7d083cc6d9
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-31 19:13:01 +0530

    add files & example for method overloading in polymorphism

---
+++ Polymorphism2/MeOv.java
@@ -0,0 +1,19 @@
+public class MeOv {
+	void add(int x, int y) {
+		System.out.println("x + y = "+(x + y));
+	}
+	void add(int x, int y, int z ) {
+		System.out.println("x + y + z = "+(x + y + z));
+	}
+	void add(int x, int y, int z, int a) {
+		System.out.println("x + y + z + a = "+ (x + y + z + a));
+	}
+	
+	public static void main(String args []) {
+		MeOv  m1 = new MeOv();
+		m1.add(10, 20);
+		m1.add(10,20,30);
+		m1.add(10,20,30,40);
+		
+	}
+}
