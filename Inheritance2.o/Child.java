079617f8149c30e9f05e556ee174f8895e1abf28
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-01 13:44:26 +0530

    remove access specifier in parent class

--- Inheritance2.o/Child.java
+++ Inheritance2.o/Child.java
@@ -1,21 +1,40 @@
-class Base {
-    int x; 
-    int y;
-    void show() {
-        System.out.println(x);
-        System.out.println(y);
-    }
-}
-public class Child extends Base {
+// class Base {
+//     int x; 
+//     int y;
+//     void show() {
+//         System.out.println(x);
+//         System.out.println(y);
+//     }
+// }
+// public class Child extends Base {
 
-     void get(int x, int y){
-        this.x = x;
-        this.y = y;
+//      void get(int x, int y){
+//         this.x = x;
+//         this.y = y;
+//      }
+
+//      public static void main(String[] args) {
+//         Child c1 = new Child();
+//         c1.get(10, 20);
+//         c1.show();
+//      }
+// }
+
+// super Keyworkd:
+class Base {
+
+     int x = 10;
+}
+public class Child extends Base{
+
+    int x = 20;
+     void show() {
+        System.out.println("x : "+x);
+
      }
 
      public static void main(String[] args) {
         Child c1 = new Child();
-        c1.get(10, 20);
         c1.show();
      }
 }
