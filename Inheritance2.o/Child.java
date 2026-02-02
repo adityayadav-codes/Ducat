1eb2d091fde9eb802f278cd86a039eee9ad8ef0f
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-02 16:33:24 +0530

    update code

--- Inheritance2.o/Child.java
+++ Inheritance2.o/Child.java
@@ -1,114 +1,114 @@
-// class Base {
-//     int x; 
-//     int y;
-//     void show() {
-//         System.out.println(x);
-//         System.out.println(y);
-//     }
-// }
-// public class Child extends Base {
+class Base {
+    int x; 
+    int y;
+    void show() {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+public class Child extends Base {
 
-//      void get(int x, int y){
-//         this.x = x;
-//         this.y = y;
-//      }
+     void get(int x, int y){
+        this.x = x;
+        this.y = y;
+     }
 
-//      public static void main(String[] args) {
-//         Child c1 = new Child();
-//         c1.get(10, 20);
-//         c1.show();
-//      }
-// }
+     public static void main(String[] args) {
+        Child c1 = new Child();
+        c1.get(10, 20);
+        c1.show();
+     }
+}
 
-// // super Keyworkd:
-// class Base {
+super Keyworkd:
+class Base {
 
-//      int x = 10;
-// }
-// public class Child extends Base{
+     int x = 10;
+}
+public class Child extends Base{
 
-//     int x = 20;
-//      void show() {
-//         System.out.println("x : "+x);
-//         System.out.println("x : "+(this.x));
+    int x = 20;
+     void show() {
+        System.out.println("x : "+x);
+        System.out.println("x : "+(this.x));
 
-//      }
+     }
 
-//      public static void main(String[] args) {
-//         Child c1 = new Child();
-//         c1.show();
-//      }
-// }
+     public static void main(String[] args) {
+        Child c1 = new Child();
+        c1.show();
+     }
+}
 
 
-// ======================Stronger to Weaker (Access Specifier)===============
-// class Base {
-//    protected void show() {
-//         System.out.println("show from Base class");
-//     }
-// }
-// public class Child  extends Base{
+======================Stronger to Weaker (Access Specifier)===============
+class Base {
+   protected void show() {
+        System.out.println("show from Base class");
+    }
+}
+public class Child  extends Base{
 
-//   public void show () {
-//         System.out.println("show from child class");
-//         super.show();
-//     }
+  public void show () {
+        System.out.println("show from child class");
+        super.show();
+    }
 
-//     public static void main(String [] args) {
+    public static void main(String [] args) {
 
-//         Child c1 = new Child();
-//         c1.show();
-//     }
+        Child c1 = new Child();
+        c1.show();
+    }
     
     
-// }
+}
 
 
 // Access Modifier :-
 
-// class Base {
+class Base {
 
-//         static void show() {
-//             System.out.println("show from Base clss");
-//         }
+        static void show() {
+            System.out.println("show from Base clss");
+        }
 
-// }
-// public class Child extends Base{
+}
+public class Child extends Base{
 
-//     static void show() {
-//         System.out.println("show from Child class");
+    static void show() {
+        System.out.println("show from Child class");
    
-//     }
+    }
 
-//     public static void main(String [] args) {
+    public static void main(String [] args) {
 
-//         Child c1 = new Child();
-//         c1.show();
-//     }
-// }
+        Child c1 = new Child();
+        c1.show();
+    }
+}
 
 
 // upcasting & downcasting
 
-// class Base {
-//     void show() {
-//         System.out.println("show from base class");
-//     }
-// }
-// class Child extends Base {
+class Base {
+    void show() {
+        System.out.println("show from base class");
+    }
+}
+class Child extends Base {
 
-//     void show() {
-//         System.out.println("show from child class");
-//     }
-//     void display() {
-//         System.out.println("Display from child class");
-//     }
+    void show() {
+        System.out.println("show from child class");
+    }
+    void display() {
+        System.out.println("Display from child class");
+    }
 
-//     public static void main(String[] args) {
-//         Base c1 = new Child(); // upcasting
-//         c1.show();
-//     }
-// }
+    public static void main(String[] args) {
+        Base c1 = new Child(); // upcasting
+        c1.show();
+    }
+}
 
 class Base {
     void show() {
