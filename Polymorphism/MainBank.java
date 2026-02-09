706122fd960a0ceed7c941204f7230f56a34a861
adityayadav-codes <adityayadav2905@gmail.com>
2026-02-09 10:00:00 +0530

    update commit

--- Polymorphism/MainBank.java
+++ Polymorphism/MainBank.java
@@ -11,17 +11,7 @@ class SavingAccount extends Bank {
     @Override
         public void withdraw(){
             System.out.println("Withdraw Amount from Savings Account");
-}
-class CurrentAccount extends Bank {
-    @Override
-        public void deposit(){
-            System.out.println("Depositing Amount in Current Accounts");    
-        }
-    @Override
-        public void withdraw(){
-            System.out.println("Withdraw Amount from Current Account");
-        }
-    } 
+    }   
 }
 public class MainBank {
         public static void main (String args [] ) {
