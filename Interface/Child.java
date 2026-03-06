24db15685e063c9e7f6047d6da5c1fdbe50f48e7
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-06 15:05:43 +0530

    Non- static method in interface

--- Interface/Child.java
+++ Interface/Child.java
@@ -1,10 +1,27 @@
+// interface My {
+//     static void show() {
+//         System.out.println("show interface from My");
+//     }
+// }
+// public class Child implements My {
+//     public static void main (String [] args) {
+//         My.show();
+//     }
+// }
+
+// Non - static method in interface
 interface My {
-    static void show() {
-        System.out.println("show interface from My");
+    default void show() {
+        System.out.println("Show from interface My");
     }
 }
-public class Child implements My {
-    public static void main (String [] args) {
-        My.show();
+public  class Child implements My {
+    public void show() {
+        System.out.println("Show from child class");
     }
-}
+    public static void main(String [] args) {
+        
+        Child c1 = new Child();
+        c1.show();
+    }
+} 
