dace5f0c83dd8c29426e2c7f9b6bf81421cba0d2
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-06 22:39:07 +0530

    update code

--- Interface/Child.java
+++ Interface/Child.java
@@ -1,74 +1,74 @@
-// interface My {
-//     static void show() {
-//         System.out.println("show interface from My");
-//     }
-// }
-// public class Child implements My {
-//     public static void main (String [] args) {
-//         My.show();
-//     }
-// }
+interface My {
+    static void show() {
+        System.out.println("show interface from My");
+    }
+}
+public class Child implements My {
+    public static void main (String [] args) {
+        My.show();
+    }
+}
 
-// // Non - static method in interface
-// interface My {
-//     default void show() {
-//         System.out.println("Show from interface My");
-//     }
-// }
-// public  class Child implements My {
-//     public void show() {
-//         System.out.println("Show from child class");
-//     }
-//     public static void main(String [] args) {
+// Non - static method in interface
+interface My {
+    default void show() {
+        System.out.println("Show from interface My");
+    }
+}
+public  class Child implements My {
+    public void show() {
+        System.out.println("Show from child class");
+    }
+    public static void main(String [] args) {
         
-//         Child c1 = new Child();
-//         c1.show();
-//     }
-// } 
+        Child c1 = new Child();
+        c1.show();
+    }
+} 
 
-// //Default Mehtod in interface
-// interface My {
-//     void show();
-// }
-// class Child implements My {
-//     public void show() {
-//         System.out.println("show from child ");
-//     }
+//Default Mehtod in interface
+interface My {
+    void show();
+}
+class Child implements My {
+    public void show() {
+        System.out.println("show from child ");
+    }
 
-//     public static void main(String [] args) {
-//         Child c1 = new Child();
-//         c1.show();
-//     }
-// }
+    public static void main(String [] args) {
+        Child c1 = new Child();
+        c1.show();
+    }
+}
 
-// //Multiple Inheritance via Interface
+//Multiple Inheritance via Interface
 
-// interface My {
-//     void show();
-// }
-// interface Mine {
-//     void show();
-// }
-// public class Child implements My, Mine {
-//     public void show() {
-//         System.out.println("show from class Child");
-//     }
-//     public static void main(String [] args) {
-//         Child c1 = new Child();
-//         c1.show();
-//     }
-// }
+interface My {
+    void show();
+}
+interface Mine {
+    void show();
+}
+public class Child implements My, Mine {
+    public void show() {
+        System.out.println("show from class Child");
+    }
+    public static void main(String [] args) {
+        Child c1 = new Child();
+        c1.show();
+    }
+}
 
 
-// Data Member in Inheritance
+Data Member in Inheritance
 
-// interface My {
-//     int x = 10;
-// }
-// public class Child implements My {
-//     public static void main(String [] args) {
+interface My {
+    int x = 10;
+}
+public class Child implements My {
+    public static void main(String [] args) {
 
-//         System.out.println(x);
-//        // x = x+10;  by default assign a value to static final variable so doesn't changable
-//     }
-// }
+        System.out.println(x);
+       // x = x+10;  by default assign a value to static final variable so doesn't changable
+    }
+}
