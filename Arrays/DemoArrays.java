efcd79e27611846c4fb79ffa230a608f403f8242
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-07 10:10:19 +0530

    multidimensional Arrays

--- Arrays/DemoArrays.java
+++ Arrays/DemoArrays.java
@@ -1,11 +1,30 @@
-public class DemoArrays {
+// public class DemoArrays {
 
+//     public static void main(String [] args) {
+
+//         int [] x = {10,20,30,40,50};
+
+//         for(int i =0; i < x.length; i++) {
+//             System.out.println(x[i]);
+//         }
+//     }
+// }
+
+// Multi-dimensional Arrays
+
+public class DemoArrays {
     public static void main(String [] args) {
 
-        int [] x = {10,20,30,40,50};
-
+        int [][]x = {{10,20,30}, {20,40,60}, {10,30,70}};
+        
         for(int i =0; i < x.length; i++) {
-            System.out.println(x[i]);
-        }
+
+            for(int j =0; j < x[i].length; j++) {
+
+                System.out.print(x[i][j]+ " ");
+            }
+
+            System.out.println();
+         }
     }
 }
