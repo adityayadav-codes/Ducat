30b323c85f43816caf6ae472d8ae3c7c1cfdcc1a
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-07 09:50:35 +0530

    Simple Single dimension Arrays

---
+++ Arrays/DemoArrays.java
@@ -0,0 +1,11 @@
+public class DemoArrays {
+    public static void main(String [] args) {
+
+        int x [] = {10,20,30,40,50};
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[3]);
+        System.out.println(x[4]);
+        //System.out.println(x[5]);  // out of bound 
+    }
+}
