d0e4fba49a6e3401f8a451ec97e929a43c4b3ccb
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-07 09:52:45 +0530

    Single dimension arrays with loop

--- Arrays/DemoArrays.java
+++ Arrays/DemoArrays.java
@@ -1,11 +1,11 @@
 public class DemoArrays {
+
     public static void main(String [] args) {
 
-        int x [] = {10,20,30,40,50};
-        System.out.println(x[0]);
-        System.out.println(x[1]);
-        System.out.println(x[3]);
-        System.out.println(x[4]);
-        //System.out.println(x[5]);  // out of bound 
+        int [] x = {10,20,30,40,50};
+
+        for(int i =0; i < x.length; i++) {
+            System.out.println(x[i]);
+        }
     }
 }
