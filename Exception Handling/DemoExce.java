7bf789dc003f28b3f39b5181d5a3eda23bcd3470
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-09 10:00:00 +0530

    update code

--- Exception Handling/DemoExce.java
+++ Exception Handling/DemoExce.java
@@ -1,44 +1,44 @@
 //Arithmetic Exception===========
-// import java.util.Scanner;
-// public class DemoExce {
-//     public static void main(String [] args) {
+import java.util.Scanner;
+public class DemoExce {
+    public static void main(String [] args) {
 
-//         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-//         try {
+        try {
 
-//             System.out.println("Enter the first number: ");
-//             int a = sc.nextInt();
+            System.out.println("Enter the first number: ");
+            int a = sc.nextInt();
             
-//             System.out.println("Enter the second number: ");
-//             int b = sc.nextInt();
+            System.out.println("Enter the second number: ");
+            int b = sc.nextInt();
 
-//             int result = a/b;
-//             System.out.println("Result : "+result);
-//         }
-//         catch(ArithmeticException e){
+            int result = a/b;
+            System.out.println("Result : "+result);
+        }
+        catch(ArithmeticException e){
 
-//             System.out.println("Cannot divide by zero!");
-//         }
-//     }
-// }
+            System.out.println("Cannot divide by zero!");
+        }
+    }
+}
 
-// Array Index out o Bound Exception ===================
+//Array Index out o Bound Exception ===================
 
-// public class DemoExce {
-//     public static void main(String[] args) {
+public class DemoExce {
+    public static void main(String[] args) {
         
-//         int arr[] = {10,20,30,40};
+        int arr[] = {10,20,30,40};
 
-//         try {
-//             System.out.println(arr[8]);
-//         }
-//         catch(ArrayIndexOutOfBoundsException e) {
+        try {
+            System.out.println(arr[8]);
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
 
-//             System.out.println("Array index out of range");
-//         }
-//     }
-// }
+            System.out.println("Array index out of range");
+        }
+    }
+}
 
 // Example 2 =======
 public class DemoExce {
