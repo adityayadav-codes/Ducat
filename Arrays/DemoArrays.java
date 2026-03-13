cf967ad4e8bd5e375547ebff81b3cc6108a9eee5
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-13 22:39:27 +0530

    Print Reverse Number

--- Arrays/DemoArrays.java
+++ Arrays/DemoArrays.java
@@ -12,19 +12,32 @@
 
 // Multi-dimensional Arrays
 
+// public class DemoArrays {
+//     public static void main(String [] args) {
+
+//         int [][]x = {{10,20,30}, {20,40,60}, {10,30,70}};
+        
+//         for(int i =0; i < x.length; i++) {
+
+//             for(int j =0; j < x[i].length; j++) {
+
+//                 System.out.print(x[i][j]+ " ");
+//             }
+
+//             System.out.println();
+//          }
+//     }
+// }
 public class DemoArrays {
+
     public static void main(String [] args) {
 
-        int [][]x = {{10,20,30}, {20,40,60}, {10,30,70}};
-        
-        for(int i =0; i < x.length; i++) {
+        int []arr = {1,2,3,4,5};
 
-            for(int j =0; j < x[i].length; j++) {
+        System.out.println("Reverse Number");
+        for(int i = arr.length-1; i > 0; i--) {
 
-                System.out.print(x[i][j]+ " ");
-            }
-
-            System.out.println();
-         }
+            System.out.println(arr[i]);
+        }
     }
 }
