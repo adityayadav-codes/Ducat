655178133c12e9d74572d3f74311b38199c589be
adityayadav-codes <adityayadav2905@gmail.com>
2026-04-09 10:00:00 +0530

    update code

--- Exception Handling/DemoExce.java
+++ Exception Handling/DemoExce.java
@@ -1,27 +1,27 @@
 //Arithmetic Exception===========
-import java.util.Scanner;
-public class DemoExce {
-    public static void main(String [] args) {
+// import java.util.Scanner;
+// public class DemoExce {
+//     public static void main(String [] args) {
 
-        Scanner sc = new Scanner(System.in);
+//         Scanner sc = new Scanner(System.in);
 
-        try {
+//         try {
 
-            System.out.println("Enter the first number: ");
-            int a = sc.nextInt();
+//             System.out.println("Enter the first number: ");
+//             int a = sc.nextInt();
             
-            System.out.println("Enter the second number: ");
-            int b = sc.nextInt();
+//             System.out.println("Enter the second number: ");
+//             int b = sc.nextInt();
 
-            int result = a/b;
-            System.out.println("Result : "+result);
-        }
-        catch(ArithmeticException e){
+//             int result = a/b;
+//             System.out.println("Result : "+result);
+//         }
+//         catch(ArithmeticException e){
 
-            System.out.println("Cannot divide by zero!");
-        }
-    }
-}
+//             System.out.println("Cannot divide by zero!");
+//         }
+//     }
+// }
 
 // Array Index out o Bound Exception ===================
 
